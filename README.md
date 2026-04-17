28d7e5f35a3c02d6c730f911ee1a5d9434fee5af
Anh Tester <thaian.it15@gmail.com>
2026-04-17 22:24:55 +0700

    Bài 5 Locators

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # SeleniumMaven012026
-Đây là source code dạy học phần Selenium Java cho lớp Selenium Java 01/2026
+
+Đây là dự án source code dành cho khóa học Selenium Java 01/2026. Dự án này sử dụng Selenium WebDriver với Java và Maven để thực hiện các bài tập và demo về tự động hóa trình duyệt web.
+
+## Mục lục
+- [Yêu cầu hệ thống](#yêu-cầu-hệ-thống)
+- [Cài đặt](#cài-đặt)
+- [Cấu trúc dự án](#cấu-trúc-dự-án)
+- [Cách chạy](#cách-chạy)
+- [Bài học](#bài-học)
+- [Giấy phép](#giấy-phép)
+
+## Yêu cầu hệ thống
+- Java JDK 17 hoặc cao hơn
+- Maven 3.x
+- Có cài Trình duyệt web (Chrome, Firefox, etc.)
+- IDE như IntelliJ IDEA hoặc VS Code, Antigravity
+
+## Cài đặt
+1. Clone repository này về máy:
+   ```
+   git clone https://github.com/anhtester/SeleniumMaven012026.git
+   ```
+2. Mở dự án trong IDE của bạn.
+3. Đảm bảo Maven dependencies được tải về bằng cách chạy:
+   ```
+   mvn clean install
+   ```
+
+## Cấu trúc dự án
+- `src/main/java/com/anhtester/`: Chứa code chính (ví dụ: Main.java)
+- `src/test/java/com/anhtester/`: Chứa các file test Selenium
+  - `DemoSelenium.java`: Demo cơ bản về Selenium
+  - `TestSelenium.java`: Các test case Selenium
+  - `Bai5_Locators/`: Bài học về Locators
+    - `LocatorsHTML.java`
+    - `LocatorsXPath_01_05.java`
+    - `LocatorsXPath_06_10.java`
+- `pom.xml`: File cấu hình Maven
+- `target/`: Thư mục chứa các file compiled
+
+## Cách chạy
+Để chạy các test Selenium:
+1. Đảm bảo WebDriver được cài đặt và đường dẫn được cấu hình (ví dụ: chromedriver.exe trong PATH).
+2. Chạy test từ IDE hoặc sử dụng Maven:
+   ```
+   mvn test
+   ```
+3. Để chạy một class cụ thể:
+   ```
+   mvn test -Dtest=DemoSelenium
+   ```
+
+## Bài học
+Dự án này bao gồm các bài học về:
+- Cài đặt và cấu hình Selenium WebDriver
+- Tương tác với các element trên trang web
+- Sử dụng Locators (HTML, XPath)
+- Viết test case tự động
+
+## Giấy phép
+Dự án này được phân phối dưới giấy phép MIT. Xem file [LICENSE](LICENSE) để biết thêm chi tiết.
---
+++ src/test/java/com/anhtester/Bai5_Locators/LocatorsXPath_01_05.java
@@ -0,0 +1,52 @@
+package com.anhtester.Bai5_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LocatorsXPath_01_05 {
+   public static void main(String[] args) throws InterruptedException {
+
+      WebDriver driver = new ChromeDriver();
+
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+      driver.get("https://crm.anhtester.com/admin/authentication");
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//input[@type='email']")).sendKeys("admin@example.com");
+      driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//span[normalize-space()='Customers']")).click();
+      driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).click();
+      driver.findElement(By.xpath("//input[@id='company']")).sendKeys("AN_TEST_1704A1");
+      driver.findElement(By.xpath("//input[@id='vat']")).sendKeys("10");
+      driver.findElement(By.xpath("//input[@id='phonenumber']")).sendKeys("0939206009");
+      driver.findElement(By.id("website")).sendKeys("https://anhtester.com");
+      driver.findElement(By.xpath("//button[@data-id='groups_in[]']")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[@data-id='groups_in[]']/following-sibling::div//input")).sendKeys("VIP");
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//span[normalize-space()='VIP']")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[@data-id='groups_in[]']")).click();
+      driver.findElement(By.xpath("//textarea[@id='address']")).sendKeys("123 Nguyen Van Linh, Da Nang");
+      driver.findElement(By.xpath("//input[@id='city']")).sendKeys("Da Nang");
+      driver.findElement(By.xpath("//input[@id='state']")).sendKeys("Da Nang");
+      driver.findElement(By.id("zip")).sendKeys("550000");
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[@data-id='country']")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[@data-id='country']/following-sibling::div//input")).sendKeys("Vietnam");
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//span[normalize-space()='Vietnam']")).click();
+      Thread.sleep(3000);
+
+      driver.quit();
+
+   }
+}
